155f1b27982eee497a9248a360e4c758abfd9d30
Kat <kat@kat.(none)>
2013-04-06 13:18:32 -0700

    model schema db all set

---
+++ schema.sql
@@ -0,0 +1,15 @@
+create table Users (
+	id INTEGER PRIMARY KEY,
+	email VARCHAR(64),
+	password VARCHAR(64),
+	name VARCHAR(64)
+);
+create table Participants (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(64),
+	chances INTEGER
+);
+create table Winners(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(64)
+);
